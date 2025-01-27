dd844c27b464f704732ccb52b217028ee766ec84
WhirlyFan <mhykoly@gmail.com>
2025-01-26 20:03:07 -0800

    Enhance job details page by adding company name, location, and requirements to the display

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -4,17 +4,21 @@ import { useData } from "@/context/DataContext";
 import { useParams } from "next/navigation";
 
 export default function JobsDetails() {
-  const { data, loading, refetch } = useData();
+  const { data, loading } = useData();
   const params = useParams();
   const id = params.id as string;
   const details = data[parseInt(id)];
   if (loading) {
     return <div>Loading...</div>;
   }
+  // Job Title,Company Name,Location,Job Description,Requirements
   return (
     <div>
       <h1>{details.jobTitle}</h1>
+      <h2>{details.companyName}</h2>
+      <h3>{details.location}</h3>
       <p>{details.jobDescription}</p>
+      <p>{details.requirements}</p>
     </div>
   );
 }
